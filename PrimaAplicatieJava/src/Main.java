3b9ab6dbc0bb9783643a3222c06482536a383cd3
Cristea Bogdan <cristea.ibogdan@gmail.com>
2022-08-30 19:20:13 +0300

    Prima incercare

---
+++ PrimaAplicatieJava/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    //Jar - Java Archive
+    //Java_Home
+    public static void main(String[] args) {
+        System.out.println("Invat limbajul Java si despre masina virtuala Java!");
+    }
+}
