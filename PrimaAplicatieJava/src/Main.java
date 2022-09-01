03f3e09562da400fcf711842a0e99a8c4d13c9a7
Cristea Bogdan <cristea.ibogdan@gmail.com>
2022-09-01 14:15:40 +0300

    Modificare Main.java

--- PrimaAplicatieJava/src/Main.java
+++ PrimaAplicatieJava/src/Main.java
@@ -1,5 +1,4 @@
 public class Main {
-    //Jar - Java Archive
     //Java_Home
     public static void main(String[] args) {
         System.out.println("Invat limbajul Java si despre masina virtuala Java!");
