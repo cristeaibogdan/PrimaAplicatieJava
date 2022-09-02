94b83ea81aedf36f351bfffdc8d4eabf89a8d592
Cristea Bogdan <cristea.ibogdan@gmail.com>
2022-09-02 10:03:10 +0300

    Adaugare comentarii pe o singura linie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+
+    //Aceasta este metoda main, care porneste o aplicatie Java
     public static void main(String[] args) {
+        //Inainte de comentarius nu se poate pune text
+        //Aici vrem sa afisam pe IntelliJ un mesaj ales de noi
         System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
 }
